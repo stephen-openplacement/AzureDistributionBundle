7f0ab937d06249b5cb0e7af4e062d9656ff688d0
Benjamin Eberlei <kontakt@beberlei.de>
2012-05-07 14:55:47 +0200

    Missing namespace slash

--- Deployment/Assets/WebRoleStrategy.php
+++ Deployment/Assets/WebRoleStrategy.php
@@ -11,7 +11,7 @@
  * to kontakt@beberlei.de so I can send you a copy immediately.
  */
 
-namespace WindowsAzureDistributionBundle\Deployment\Assets;
+namespace WindowsAzure\DistributionBundle\Deployment\Assets;
 
 /**
  * Serve assets from the webrole.
